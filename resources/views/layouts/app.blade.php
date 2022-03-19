bc9b1f38aa30a9683d2ab264e24078d7dc569eea
Saimcan <saimcan.sesigur@gmail.com>
2022-03-19 18:37:13 +0300

    front-end libraries and views added
    1. included tailwind css into project
    2. included tailwind elements into project
    3. added app.blade.php layout
    4. added pokedex.index.blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pokedex</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
+</head>
+<body>
+
+
+@yield('messages')
+
+@yield('header')
+
+@yield('content')
+
+@yield('footer')
+
+<script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+</html>
--- resources/views/pokedex/index.blade.php
+++ resources/views/pokedex/index.blade.php
@@ -1,1 +1,15 @@
-test
+@extends('layouts.app')
+
+@section('header')
+    <div class="md:container md:mx-auto">
+        <h3 class="font-medium leading-tight text-5xl mt-0 mb-2 text-black text-center">List of Pokémons</h3>
+    </div>
+@endsection
+
+@section('content')
+    <h2>content</h2>
+@endsection
+
+@section('footer')
+    <h2>footer</h2>
+@endsection
